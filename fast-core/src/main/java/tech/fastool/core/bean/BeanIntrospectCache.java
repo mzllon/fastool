d85762ccc75ee113ca6644a57c2def03e70492cb
miles.tang <and.mz.yq@gmail.com>
2022-06-06 13:22:31 +0800

    Bean工具类

---
+++ fast-core/src/main/java/tech/fastool/core/bean/BeanIntrospectCache.java
@@ -0,0 +1,98 @@
+package tech.fastool.core.bean;
+
+import tech.fastool.core.exceptions.BeanException;
+import tech.fastool.core.lang.ObjectUtil;
+import tech.fastool.core.lang.SimpleCache;
+import tech.fastool.core.lang.Singletons;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Bean内省缓存
+ *
+ * @author miles.tang
+ * @version 0.0.1
+ * @date 2022-04-09
+ */
+public class BeanIntrospectCache {
+
+    /**
+     * 缓存已经内省过的Class
+     */
+    private final SimpleCache<Class<?>, List<PropertyDescriptor>> descriptorsCache = new SimpleCache<>(1024);
+
+    private BeanIntrospectCache() {
+
+    }
+
+    /**
+     * 获取Javabean的属性描述列表
+     *
+     * @param beanClass 对象内容
+     * @return {@code PropertyDescriptor}数组
+     */
+    public List<PropertyDescriptor> getPropertyDescriptors(Class<?> beanClass) {
+        ObjectUtil.requireNonNull(beanClass, "BeanClass == null");
+        List<PropertyDescriptor> propertyDescriptorList;
+        synchronized (descriptorsCache) {
+            propertyDescriptorList = descriptorsCache.get(beanClass);
+        }
+        if (propertyDescriptorList == null) {
+            try {
+                BeanInfo beanInfo = Introspector.getBeanInfo(beanClass);
+                PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+                propertyDescriptorList = new ArrayList<>(propertyDescriptors.length);
+                for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+                    if (!"class".equals(propertyDescriptor.getName())) {
+                        propertyDescriptorList.add(propertyDescriptor);
+                    }
+                }
+                synchronized (descriptorsCache) {
+                    descriptorsCache.put(beanClass, propertyDescriptorList);
+                }
+            } catch (IntrospectionException e) {
+                throw new BeanException(String.format("Failed to obtain BeanInfo for class [%s]", beanClass.getName()), e);
+            }
+        }
+        return propertyDescriptorList;
+    }
+
+    /**
+     * 根据属性名称获取对应属性对象
+     *
+     * @param beanClass    类型
+     * @param propertyName 属性名
+     * @return {@linkplain PropertyDescriptor}
+     */
+    public PropertyDescriptor getPropertyDescriptor(Class<?> beanClass, String propertyName) {
+        List<PropertyDescriptor> propertyDescriptors = this.getPropertyDescriptors(beanClass);
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+            if (propertyDescriptor.getName().equals(propertyName)) {
+                return propertyDescriptor;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 清理缓存
+     */
+    public void clearDescriptors() {
+        descriptorsCache.clear();
+    }
+
+    /**
+     * 返回{@linkplain BeanIntrospectCache}单例
+     *
+     * @return {@linkplain BeanIntrospectCache} Singleton
+     */
+    public static BeanIntrospectCache getInstance() {
+        return Singletons.get(BeanIntrospectCache.class);
+    }
+
+}
---
+++ fast-core/src/main/java/tech/fastool/core/bean/CopyOption.java
@@ -0,0 +1,102 @@
+package tech.fastool.core.bean;
+
+import tech.fastool.core.lang.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Bean复制的配置
+ *
+ * @author miles.tang
+ * @version 0.0.1
+ * @date 2022-04-09
+ */
+public class CopyOption implements java.io.Serializable {
+
+    private static final long serialVersionUID = 2021L;
+
+    /**
+     * 是否忽略空值，当源对象的值为null时，true: 忽略而不注入此值，false: 注入null
+     */
+    private boolean ignoreNullValue = false;
+
+    /**
+     * 忽略空字符串
+     */
+    private boolean ignoreEmptyString = false;
+
+    /**
+     * 忽略的目标对象中属性列表，设置一个属性列表，不拷贝这些属性值
+     */
+    private List<String> ignoreProperties = new ArrayList<>();
+
+    public boolean isIgnoreNullValue() {
+        return ignoreNullValue;
+    }
+
+    public boolean isIgnoreEmptyString() {
+        return ignoreEmptyString;
+    }
+
+    public List<String> getIgnoreProperties() {
+        return ignoreProperties;
+    }
+
+    /**
+     * 默认构造器
+     */
+    public CopyOption() {
+
+    }
+
+    /**
+     * 是否忽略空值的构造器
+     *
+     * @param ignoreNullValue 是否忽略空值
+     */
+    public CopyOption(boolean ignoreNullValue) {
+        this();
+        this.ignoreNullValue = ignoreNullValue;
+    }
+
+    /**
+     * 需要忽略的属性名列表的构造器
+     *
+     * @param ignoreProperties 忽略的目标对象中属性列表，设置一个属性列表，不拷贝这些属性值
+     */
+    public CopyOption(String... ignoreProperties) {
+        this();
+        if (ArrayUtil.isNotEmpty(ignoreProperties)) {
+            this.ignoreProperties.addAll(Arrays.asList(ignoreProperties));
+        }
+    }
+
+    /**
+     * 构造器
+     *
+     * @param ignoreNullValue  是否忽略空值
+     * @param ignoreProperties 忽略的目标对象中属性列表，设置一个属性列表，不拷贝这些属性值
+     */
+    public CopyOption(boolean ignoreNullValue, String... ignoreProperties) {
+        this(ignoreProperties);
+        this.ignoreNullValue = ignoreNullValue;
+    }
+
+    public CopyOption setIgnoreNullValue(boolean ignoreNullValue) {
+        this.ignoreNullValue = ignoreNullValue;
+        return this;
+    }
+
+    public CopyOption setIgnoreEmptyString(boolean ignoreEmptyString) {
+        this.ignoreEmptyString = ignoreEmptyString;
+        return this;
+    }
+
+    public CopyOption setIgnoreProperties(List<String> ignoreProperties) {
+        this.ignoreProperties = ignoreProperties;
+        return this;
+    }
+
+}
